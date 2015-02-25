7da0e945d9ae81ad01ea24aa076bad4acec24538
joss <josselin54.chevalay@gmail.com>
2015-02-25 14:08:23 +0100

    add route for logout

--- app/Routes/Oauth.js
+++ app/Routes/Oauth.js
@@ -1,3 +1,12 @@
+
+
+// ensure auth
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.status(403);
+  res.send();
+}
+
 module.exports = function(app, passport) {
   app.get('/auth/facebook', passport.authenticate('facebook'), function(req, resp){});
   app.get('/auth/facebook/callback',
@@ -5,4 +14,8 @@ module.exports = function(app, passport) {
     function(req, res) {
      res.redirect('/');
     });
+  app.get('/auth/logout', ensureAuthenticated, function(req, resp){
+    req.session.passport.user = null;
+    resp.redirect('/');
+  });
 };
