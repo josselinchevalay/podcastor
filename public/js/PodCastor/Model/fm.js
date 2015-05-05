8a6be7ccc85702ca26b8cb62adb88baf98fa480a
joss <josselin54.chevalay@gmail.com>
2015-05-05 12:08:27 +0200

    change model and service for fm

---
+++ public/js/PodCastor/Model/fm.js
@@ -0,0 +1,16 @@
+/**
+ * Model 
+ * @author josselin chevalay
+ * @package PodCastor/Model
+ */
+ 
+ var Fm = function() {
+	this.name 			= null;
+	this.description 	= null;
+	this.author 		= null;
+	this.type 			= 'Fm';
+	this.like 			= [];
+	this.dislike 		= [];
+	this.followers 		= [];
+	this.podcasts 		= [];
+ };
